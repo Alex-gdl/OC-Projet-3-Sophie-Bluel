094efa650b97a867bb19a4b8b04d4694c5e9a92c
Alex-gdl <138564606+Alex-gdl@users.noreply.github.com>
2023-11-09 18:34:15 +0100

    login changes

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,43 +2,74 @@ const loginInfo = document.querySelector(".login-info");
 const messageError = document.getElementById("error");
 const loginURL = "http://localhost:5678/api/users/login";
 
-loginInfo.addEventListener("submit", function (event) {
+loginInfo.addEventListener("submit", handleLogin);
+
+function handleLogin(event) {
   event.preventDefault();
 
   const { email, password } = loginInfo.elements;
 
-  if (email.value === "" || password.value === "") {
-    messageError.style.display = "flex";
+  if (isFieldEmpty(email.value) || isFieldEmpty(password.value)) {
+    displayError("Erreur");
     return;
   }
 
-  messageError.style.display = "none";
+  hideError();
 
+  const credentials = {
+    email: email.value,
+    password: password.value,
+  };
+
+  sendLoginRequest(credentials);
+}
+
+function isFieldEmpty(value) {
+  return value.trim() === "";
+}
+
+function displayError(message) {
+  messageError.textContent = message;
+  messageError.style.display = "flex";
+}
+
+function hideError() {
+  messageError.style.display = "none";
+}
+
+function sendLoginRequest(credentials) {
   fetch(loginURL, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value,
-    }),
+    body: JSON.stringify(credentials),
   })
-    .then((response) => response.json())
-    .then((data) => {
-      localStorage.setItem("auth", JSON.stringify(data));
-      const auth = JSON.parse(localStorage.getItem("auth"));
+    .then(handleResponse)
+    .catch(handleError);
+}
 
-      if (auth && auth.token) {
-        window.location = "index.html";
-      } else {
-        messageError.style.display = "flex";
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      messageError.style.display = "flex";
-    });
-});
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json().then(handleSuccess);
+  } else {
+    return response.json().then(handleFailure);
+  }
+}
 
+function handleSuccess(data) {
+  localStorage.setItem("auth", JSON.stringify(data));
+  const auth = JSON.parse(localStorage.getItem("auth"));
+
+  if (auth && auth.token) {
+    window.location = "index.html";
+  } else {
+    displayError("Erreur");
+  }
+}
+
+function handleFailure(error) {
+  console.error("Error:", error);
+  displayError("Erreur");
+}
